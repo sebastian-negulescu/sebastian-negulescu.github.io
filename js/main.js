02f04e75b522cdf4aaa18dd5587972f7387a62ff
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2021-01-17 19:09:53 -0500

    made welcome file and added all needed things

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-import Typed from '../node_modules/typed.js/src/typed.js';
+const fileUrl = '../welcome.file';
 
+const options = {
+    strings: ['display ^750welcome.file'],
+    autoInsertCss: false,
+    startDelay: 1000,
+    typeSpeed: 100,
+    onComplete: (self) => {
+        const cursor = document.querySelector('.typed-cursor');
+        cursor.style.animation = 'blink 1.2s infinite steps(1, start)';
+        const tag = document.createElement('span');
+        tag.innerHTML = '<em class="green">user@device</em>:<em class="blue">~</em>$ ';
+        document.querySelector('#main').appendChild(tag);
+        document.querySelector('#main').appendChild(cursor);
+        fetch(fileUrl)
+            .then( r => r.text() )
+            .then( t => document.querySelector('#message').innerHTML = t);
+    }
+};
+
+const typed = new Typed('#typed', options);

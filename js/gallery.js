6cb19da01304a7cf086363b05856ade809598fd8
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2022-08-07 17:45:36 -0400

    updated website

---
+++ js/gallery.js
@@ -0,0 +1,88 @@
+// for loading images
+
+load = (src) => {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.addEventListener('load', resolve);
+        image.addEventListener('error', reject);
+        image.src = src;
+    });
+};
+
+// for creating the gallery pictures
+
+const createPictures = () => {
+    const amount = 18;
+    let sum = 0;
+    const desiredSum = amount * (amount + 1) / 2;
+    const body = document.querySelector('body');
+    for (let i = 1; i <= amount; ++i) {
+        const picture = document.createElement('div');
+        const image = `./images/gallery-${i}.jpg`;
+        load(image).then(() => {
+            const ind = parseInt(image.replace('./images/gallery-', '').replace('.jpg', ''));
+
+            picture.style.backgroundImage = 'url(' + image + ')';
+            picture.onclick = (event) => {
+                const attr = event.target.style.backgroundImage;
+                const i = parseInt(attr.slice(attr.lastIndexOf('gallery-') + 8, -6));
+                openFullscreen(i);
+            };
+            document.querySelector('#gallery').appendChild(picture);
+
+            sum += ind;
+            if (sum == desiredSum) {
+                body.style.display = 'block';
+                document.querySelector('#loading').style.display = 'none';
+            }
+        });
+    }
+};
+
+createPictures();
+
+// for changing the row sizes in the gallery
+
+const modifyGallery = () => {
+    const gallery = document.querySelector('#gallery');
+    if (window.innerWidth < 1500) {
+        if (window.innerWidth < 600) {
+            gallery.style.gridTemplateColumns = 'repeat(1, 1fr)';
+            gallery.style.gridAutoRows = `calc(((${window.innerWidth}px - 4em) - (2em * (1 - 1))) / 1)`;
+        } else if (window.innerWidth < 1000) {
+            gallery.style.gridTemplateColumns = 'repeat(2, 1fr)';
+            gallery.style.gridAutoRows = `calc(((${window.innerWidth}px - 4em) - (2em * (2 - 1))) / 2)`;
+        } else {
+            gallery.style.gridTemplateColumns = 'repeat(3, 1fr)';
+            gallery.style.gridAutoRows = `calc(((${window.innerWidth}px - 4em) - (2em * (3 - 1))) / 3)`;
+        }
+    } else {
+        gallery.style.gridTemplateColumns = 'repeat(3, 1fr)';
+        gallery.style.gridAutoRows = `calc((1500px - (2em * (3 - 1))) / 3)`;
+    }
+};
+
+window.addEventListener('resize', modifyGallery);
+modifyGallery();
+
+// open fullscreen image
+
+const openFullscreen = (ind) => {
+    document.querySelector('#loading').style.display = 'block';
+    document.querySelector('#fullscreen').style.display = 'flex';
+    document.querySelector('body').style.overflow = 'hidden';
+    const image = `./images-full/full-${ind}.jpg`;
+    load(image).then(() => {
+        document.querySelector('#fullPicture').src = image;
+        document.querySelector('#fullPicture').style.display = 'block';
+        document.querySelector('#loading').style.display = 'none';
+    });
+};
+
+// close fullscreen image
+
+const closeFullscreen = () => {
+    document.querySelector('#fullscreen').style.display = 'none';
+    document.querySelector('#fullPicture').style.display = 'none';
+    document.querySelector('body').style.overflow = 'auto';
+};
--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
+// for loading background image
+
+load = (src) => {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.addEventListener('load', resolve);
+        image.addEventListener('error', reject);
+        image.src = src;
+    });
+};
+
+const image = './images/background-image-min.jpg';
+load(image).then(() => {
+    const body = document.querySelector('body');
+    body.style.backgroundImage = 'url(' + image + ')';
+    body.style.display = 'flex';
+    document.querySelector('#loading').style.display = 'none';
+});
+
+// for terminal height
+
 const resetHeight = () => {
     document.body.style.height = window.innerHeight + 'px';
+    const container = document.querySelector('#container');
+    const main = document.querySelector('#main');
 
-    var mq1 = window.matchMedia( "(max-width: 600px)" );
+    let mq1 = window.matchMedia('(max-width: 600px)');
     if (mq1.matches) { // mobile portrait
-        document.querySelector('#main').style.height = '100%';
+        container.style.height = '100%';
+        main.style.height = 'calc(100% - 7em)';
     } else {
-        var mq2 = window.matchMedia( "(min-height: 700px)" );
+        let mq2 = window.matchMedia( "(min-height: 798px)" );
         if (mq2.matches) { // desktop
-            document.querySelector('#main').style.height = '600px';
+            container.style.height = 'calc(7em + 600px)';
+            main.style.height = '600px';
         } else { // mobile landscape
-            document.querySelector('#main').style.height = '100%';
+            container.style.height = '100%';
+            main.style.height = 'calc(100% - 7em)';
         }
     }
 }
 window.addEventListener('resize', resetHeight); // add height options
 resetHeight();
 
-const getImages = async () => {
-    await fetch('../images/background-image.jpg')
-        .then( res => {
-            document.querySelector('body').style.backgroundImage = 'url("../images/background-image.jpg")';
-        });
-};
-
-getImages(); // load background
+// for typed effect
 
 const fileUrl = '../welcome.file';
 
@@ -41,7 +60,7 @@ const options = {
         document.querySelector('#main').appendChild(cursor);
         fetch(fileUrl)
             .then( r => r.text() )
-            .then( t => document.querySelector('#message').innerHTML = t);
+            .then( t => document.querySelector('#message').innerHTML = t );
     }
 };
 
